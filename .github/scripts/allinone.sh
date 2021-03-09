07895bc6c09c1a08dce33c533b63abd71f1a9174
Ivan Jobs <ruhe@microsoft.com>
2021-03-09 13:22:03 +0800

    chore: try to release 0.0.7+botTsDefault

--- .github/scripts/allinone.sh
+++ .github/scripts/allinone.sh
@@ -8,14 +8,11 @@ RAW_TAG=$1
 
 if [[ ${RAW_TAG} == *"botJsDefault"* ]];then
     cd ./bot/js
-    zip -r ../../bot_JavaScript_default_${RAW_TAG}.zip .
-    cd ../..
 elif [[ ${RAW_TAG} == *"botTsDefault"* ]];then
     cd ./bot/ts
-    zip -r ../../bot_TypeScript_default_${RAW_TAG}.zip .
-    cd ../..
 else # botCsDefault
     cd ./bot/csharp
-    zip -r ../../bot_CSharp_default_${RAW_TAG}.zip .
-    cd ../..
 fi
+
+zip ../../${RAW_TAG}.zip .
+cd ../..
