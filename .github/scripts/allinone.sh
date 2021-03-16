e6cdeda1910610ab14ddb27f8dc40ea6fc41d7fe
zhijie <zhihuan@microsoft.com>
2021-03-16 11:39:26 +0800

    fix: elif

--- .github/scripts/allinone.sh
+++ .github/scripts/allinone.sh
@@ -7,9 +7,9 @@ RAW_TAG=$1
 # get folder path to zip.
 if [[ ${RAW_TAG} == *"tab.JavaScript.default"* ]];then
     cd ./tab/js/default
-if [[ ${RAW_TAG} == *"tab.JavaScript.with-function"* ]];then
+elif [[ ${RAW_TAG} == *"tab.JavaScript.with-function"* ]];then
     cd ./tab/js/with-function
-if [[ ${RAW_TAG} == *"bot.JavaScript.default"* ]];then
+elif [[ ${RAW_TAG} == *"bot.JavaScript.default"* ]];then
     cd ./bot/js
 elif [[ ${RAW_TAG} == *"bot.TypeScript.default"* ]];then
     cd ./bot/ts
