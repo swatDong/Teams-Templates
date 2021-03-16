b8b92c779aa8c4124362268c988167a00cfa5fe1
zhijie <zhihuan@microsoft.com>
2021-03-16 11:33:30 +0800

    release: 0.1.1+tab.JavaScript.default

--- .github/scripts/allinone.sh
+++ .github/scripts/allinone.sh
@@ -5,7 +5,10 @@ set -x
 RAW_TAG=$1
 
 # get folder path to zip.
-
+if [[ ${RAW_TAG} == *"tab.JavaScript.default"* ]];then
+    cd ./tab/js/default
+if [[ ${RAW_TAG} == *"tab.JavaScript.with-function"* ]];then
+    cd ./tab/js/with-function
 if [[ ${RAW_TAG} == *"bot.JavaScript.default"* ]];then
     cd ./bot/js
 elif [[ ${RAW_TAG} == *"bot.TypeScript.default"* ]];then
