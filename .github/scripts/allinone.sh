19109e4eba1033d2cb8b8fe8c907a3c2d017080a
Heng Zhang <74335596+henzhang-ms@users.noreply.github.com>
2021-03-18 13:37:31 +0800

    feat(pipeline): Add pipeline for function
    Add zip logic for function pipeline

--- .github/scripts/allinone.sh
+++ .github/scripts/allinone.sh
@@ -11,6 +11,12 @@ if [[ ${RAW_TAG} == *"tab.JavaScript.default"* ]];then
 elif [[ ${RAW_TAG} == *"tab.JavaScript.with-function"* ]];then
     cd ./tab/js/with-function
     zip -r ../../../${RAW_TAG}.zip .
+elif [[ ${RAW_TAG} == *"function-base.JavaScript.default"* ]];then
+    cd ./function-base/js/default
+    zip -r ../../../${RAW_TAG}.zip .
+elif [[ ${RAW_TAG} == *"function-trigger.JavaScript.HTTPTrigger"* ]];then
+    cd ./function-trigger/js/HTTPTrigger
+    zip -r ../../../${RAW_TAG}.zip .
 elif [[ ${RAW_TAG} == *"bot.JavaScript.default"* ]];then
     cd ./bot/js
     zip -r ../../${RAW_TAG}.zip .
