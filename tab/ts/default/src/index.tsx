ca077006f36e8aec1477aa767f12f86b62f1c572
zhijie <zhihuan@microsoft.com>
2021-04-13 11:32:06 +0800

    feat: add ts template

---
+++ tab/ts/default/src/index.tsx
@@ -0,0 +1,13 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/App';
+import { Provider, teamsTheme } from '@fluentui/react-northstar' //https://fluentsite.z22.web.core.windows.net/quick-start
+
+ReactDOM.render(
+    <Provider theme={teamsTheme}>
+        <App />
+    </Provider>, document.getElementById('root')
+);
