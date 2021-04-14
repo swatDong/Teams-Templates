36823adf3974db1da19e3ea62d825c17ea0f11e2
zhijie <zhihuan@microsoft.com>
2021-04-14 13:34:11 +0800

    style: refine indent

--- tab/js/with-function/src/components/Config.js
+++ tab/js/with-function/src/components/Config.js
@@ -12,43 +12,43 @@ import * as microsoftTeams from "@microsoft/teams-js";
 class TabConfig extends React.Component {
 
   render() {
-      // Initialize the Microsoft Teams SDK
-      microsoftTeams.initialize();
-      /**
-       * When the user clicks "Save", save the url for your configured tab.
-       * This allows for the addition of query string parameters based on
-       * the settings selected by the user.
-       */
-      microsoftTeams.settings.registerOnSaveHandler((saveEvent) => {
+    // Initialize the Microsoft Teams SDK
+    microsoftTeams.initialize();
 
-        const baseUrl = `https://${window.location.hostname}:${window.location.port}`;
-        microsoftTeams.settings.setSettings({
-          "suggestedDisplayName": "My Tab",
-          "entityId": "Test",
-          "contentUrl": baseUrl + "/index.html#/tab",
-          "websiteUrl": baseUrl + "/index.html#/tab"
-        });
-        saveEvent.notifySuccess();
+    /**
+     * When the user clicks "Save", save the url for your configured tab.
+     * This allows for the addition of query string parameters based on
+     * the settings selected by the user.
+     */
+    microsoftTeams.settings.registerOnSaveHandler((saveEvent) => {
+      const baseUrl = `https://${window.location.hostname}:${window.location.port}`;
+      microsoftTeams.settings.setSettings({
+        "suggestedDisplayName": "My Tab",
+        "entityId": "Test",
+        "contentUrl": baseUrl + "/index.html#/tab",
+        "websiteUrl": baseUrl + "/index.html#/tab"
       });
+      saveEvent.notifySuccess();
+    });
 
-      /**
-       * After verifying that the settings for your tab are correctly
-       * filled in by the user you need to set the state of the dialog
-       * to be valid.  This will enable the save button in the configuration
-       * dialog.
-       */
-      microsoftTeams.settings.setValidityState(true);
+    /**
+     * After verifying that the settings for your tab are correctly
+     * filled in by the user you need to set the state of the dialog
+     * to be valid.  This will enable the save button in the configuration
+     * dialog.
+     */
+    microsoftTeams.settings.setValidityState(true);
 
-      return (
+    return (
+      <div>
+        <h1>Tab Configuration</h1>
         <div>
-          <h1>Tab Configuration</h1>
-          <div>
-            This is where you will add your tab configuration options the user
-            can choose when the tab is added to your team/group chat.
-          </div>
+          This is where you will add your tab configuration options the user
+          can choose when the tab is added to your team/group chat.
         </div>
-      );
-    }
+      </div>
+    );
   }
+}
 
-  export default TabConfig;
+export default TabConfig;
