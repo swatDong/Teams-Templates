68a264c33a064f3c6c046a8e04e29a1b07e1a14d
zhijie <zhihuan@microsoft.com>
2021-04-20 14:55:56 +0800

    fix: naming

--- tab/js/default/src/components/App.js
+++ tab/js/default/src/components/App.js
@@ -9,7 +9,7 @@ import { HashRouter as Router, Route } from 'react-router-dom';
 import Privacy from './about/Privacy';
 import TermsOfUse from './about/TermsOfUse';
 import Tab from './Tab';
-import TabConfig from './Config';
+import TabConfig from './TabConfig';
 
 /**
  * The main app which handles the initialization and routing
