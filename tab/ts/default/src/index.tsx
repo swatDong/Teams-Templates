a82c041c8b1744dc6a538b9ae21c4c3f4d631f3e
zhijie <zhihuan@microsoft.com>
2021-04-20 11:49:13 +0800

    fix: React must be in scope when using JSX

--- tab/ts/default/src/index.tsx
+++ tab/ts/default/src/index.tsx
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
