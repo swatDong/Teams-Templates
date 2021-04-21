597de919e1784f338448a3dee34dc319560daf61
zhijie <zhihuan@microsoft.com>
2021-04-21 14:32:49 +0800

    feat: adapt to new UI for default template

--- tab/js/default/src/components/TabConfig.js
+++ tab/js/default/src/components/TabConfig.js
@@ -40,13 +40,13 @@ class TabConfig extends React.Component {
     microsoftTeams.settings.setValidityState(true);
 
     return (
-    <div>
-      <h1>Tab Configuration</h1>
       <div>
-        This is where you will add your tab configuration options the user
-        can choose when the tab is added to your team/group chat.
+        <h1>Tab Configuration</h1>
+        <div>
+          This is where you will add your tab configuration options the user
+          can choose when the tab is added to your team/group chat.
+        </div>
       </div>
-    </div>
     );
   }
 }
